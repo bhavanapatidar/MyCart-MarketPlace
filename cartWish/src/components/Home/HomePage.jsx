52f9d8b4b77606f1ec3fd0a9f66cb15fdefc007a
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-08-05 23:02:16 +0530

    Home components

---
+++ cartWish/src/components/Home/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import iphone from '../../../assets/iphone-14-pro.webp';
+import mac from '../../../assets/mac-system-cut.jfif';
+import FeaturedProducts from './FeaturedProducts';
+
+const HomePage = () => {
+  return (
+    <div>
+        <HeroSection 
+            title="Buy iPhone 14 Pro"
+            subtitle="Experience the future of smartphones with the iPhone 14 Pro."
+            link="/products/iphone-14-pro"
+            image={iphone}
+        
+        />
+        <FeaturedProducts/>
+
+         <HeroSection 
+            title="Build the ultimaate setup with Mac"
+            subtitle="You can add Studio Display and colorful Magic Keyboard and Mouse to your Mac."
+            link="/products/iphone-14-pro"
+            image={mac}
+        
+        />       
+    </div>
+  )
+}
+
+export default HomePage
